271cb2c2307ed42bd0a84f13d6cd357022d6bfe3
emrahkiziltan94 <emrahkiziltan94@gmail.com>
2024-06-25 17:33:33 +0300

    comments are added.

--- src/components/RefDemo.tsx
+++ src/components/RefDemo.tsx
@@ -9,12 +9,13 @@ export default function RefDemo({}: IRefDemo) {
   // not setcount handle içinde çağrıldığında , count değeri return içinde print edilmemişse bile yinede sayfa render olur.
   const handleIncrement = () => {
     setCount(count + 1);
-    countRef.current++;
+    countRef.current++;  //countRef.current=count; bu şekilde kullanırsak da state in hep bir önceki değerini tutar.
 
     console.log('state: ', count);
     console.log('ref: ', countRef);
   };
-  // eğer count u aşağıda kapatırsak ref kalırsa ref in karşısındaki değerin değişmediğini görürüz çünkü ref sadece değeri tutar tekrar render sağlamaz.
+  // eğer count u aşağıda ve handleincrement içinde  kapatırsak ref kalırsa ref in karşısındaki değerin değişmediğini görürüz çünkü ref sadece değeri tutar tekrar render sağlamaz.
+  //NOT increment e bastığınızda ref değeri değişmesi count açıkken olur count sayfanın yeniden render olmasına sebep olur.
   return (
     <div className="tutorial">
       <p>Count: {count} </p>
